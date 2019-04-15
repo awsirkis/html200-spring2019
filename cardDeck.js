e439cb46302a9e7bb469ea7fc84fe50f082db0e6
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-15 16:25:15 -0700

    Create cardDeck.js

---
+++ cardDeck.js
@@ -0,0 +1,7 @@
+var suits=['H', 'C', 'S', 'D'];
+var faces=['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+for(int j = 0; j < faces.length; j++) {
+    for(int i = 0; i < suits.length; i++){
+      console.log(faces[j] + " " + suits[i]);
+    }
+}
