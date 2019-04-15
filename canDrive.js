22e7d8f258fc06ccc2069e740cf6e18d44dc7d9f
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-15 16:01:03 -0700

    Create canDrive.js

---
+++ canDrive.js
@@ -0,0 +1,9 @@
+var first = "Adam";
+var last = "Sirkis";
+var age = 19;
+const min_age = 16;
+if (age >= min_age) {
+  log(first + " " + last + "has driven " + age - min_age + " years");
+} else {
+  for (let i = 5; i >= 0; i--) log(i);
+}
