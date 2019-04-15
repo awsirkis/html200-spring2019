65cba0c3b56b53e5fe9b84ecdb37e3018915109b
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-15 16:25:30 -0700

    Update canDrive.js

--- canDrive.js
+++ canDrive.js
@@ -3,7 +3,7 @@ var last = "Sirkis";
 var age = 19;
 const min_age = 16;
 if (age >= min_age) {
-  log(first + " " + last + "has driven " + age - min_age + " years");
+  console.log(first + " " + last + "has driven " + age - min_age + " years");
 } else {
   for (let i = 5; i >= 0; i--) log(i);
 }
