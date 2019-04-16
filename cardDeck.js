00c42d0d2eeda87f92449ad121a262df19bcc1dc
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-16 16:54:57 -0700

    Update cardDeck.js

--- cardDeck.js
+++ cardDeck.js
@@ -1,5 +1,5 @@
-var suits=['H', 'C', 'S', 'D'];
-var faces=['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+var suits = ['H', 'C', 'S', 'D'];
+var faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 for(int j = 0; j < faces.length; j++) {
     for(int i = 0; i < suits.length; i++){
       console.log(faces[j] + " " + suits[i]);
