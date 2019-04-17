14b221df37aba8562fa41186cc6800fa1fe12c0c
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-16 17:06:29 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-var x = 42
-var y = "42"
-var z = false
-var w = true
-vars = [42, "42", false, true]
+const x = 42
+const y = "42"
+const z = false
+const w = true
+
 for(let i = 0; i < vars.length; i++){
   for(let j = 1; j < vars.length; j++){
     console.log(vars[i]*vars[j]);
