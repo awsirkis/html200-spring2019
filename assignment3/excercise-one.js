da344c4383aad53b66e272d32a7d0250dfd4f94c
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-18 15:34:04 -0700

    Create excercise-one.js

---
+++ assignment3/excercise-one.js
@@ -0,0 +1,8 @@
+let string_arr = ["apple", "bannana", "coconut", "grape", "orange"];
+console.log(string_arr);
+string_arr.push("mango");
+console.log(string_arr);
+string_arr.splice(2, 1);
+console.log(string_arr);
+string_arr = string_arr.join(',');
+console.log(string_arr);
