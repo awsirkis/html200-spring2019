e7b5bceca186b0c4be4a521f1b4196ed5ad0a4bb
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-04-23 10:24:27 -0700

    Update bonus-assignment-5.js

--- assignment3/bonus-assignment-5.js
+++ assignment3/bonus-assignment-5.js
@@ -1,6 +1,6 @@
-let cards = [["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"],["Spades", "Clubs", "Hearts", "Diamonds"]];
-for(i = 0; i < cards[1].length; i++){
-  for(j = 0; j < cards[0].length; j++){
-    console.log(cards[0][j] + " of " + cards[1][i]);
+let cards = [["Spades", "Clubs", "Hearts", "Diamonds"], ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]];
+for(i = 0; i < cards[0].length; i++){
+  for(j = 0; j < cards[1].length; j++){
+    console.log(cards[1][j] + " of " + cards[0][i]);
   }
 }
