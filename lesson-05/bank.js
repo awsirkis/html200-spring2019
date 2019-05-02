f3d07c4158d08d99c26557ef49996036b2f3af3e
awsirkis <44416148+awsirkis@users.noreply.github.com>
2019-05-02 12:30:08 -0700

    Add files via upload

--- lesson-05/bank.js
+++ lesson-05/bank.js
@@ -1,38 +1,76 @@
 let balance = 500;
+
 function bank() {
-  do {
-    let com = prompt("Enter a Command:");
-    switch (com) {
-      case "W":
-        let withdraw = prompt("Enter an amount to withdraw:");
-        if (withdraw > balance) {
-          prompt(
-            "Error: No withdrawing more than your current balance of $",
-            balance
-          );
-        } else if (balance - withdraw <= 300) {
-          let areUSure = confirm(
-            "Are you sure you want to leave less than $300 in your account?"
-          );
-          if (areUSure == True) {
-            balance -= withdraw;
-          }
-        } else {
-          balance -= withdraw;
+    alert("IN SCRIPT");
+    let com = '';
+    do {
+
+        com = prompt("Enter a Command:");
+
+        switch (com) {
+
+            case "W":
+
+                let withdraw = prompt("Enter an amount to withdraw:");
+
+                if (withdraw > balance) {
+
+                    alert(
+
+                      "Error: No withdrawing more than your current balance of $",
+
+                      balance
+
+                    );
+
+                } else if (balance - withdraw <= 300) {
+
+                    let areUSure = confirm(
+
+                      "Are you sure you want to leave less than $300 in your account?"
+
+                    );
+
+                    if (areUSure == True) {
+
+                        balance -= withdraw;
+
+                    }
+
+                } else {
+
+                    balance -= withdraw;
+
+                }
+
+                break;
+
+            case "D":
+
+                let deposit = prompt("Enter an amount to deposit:");
+
+                if (deposit >= 50000) {
+
+                    alert("Invalid amount: No deposits over $50,000.00");
+
+                } else {
+
+                    balance += deposit;
+
+                }
+
+                break;
+
+            case "B":
+
+                alert("Current Balance: $" +  balance.toString());
+
+                break;
+
         }
-        break;
-      case "D":
-        let deposit = prompt("Enter an amount to deposit:");
-        if (deposit >= 50000) {
-          prompt("Invalid amount: No deposits over $50,000.00");
-        } else {
-          balance += deposit;
-        }
-        break;
-      case "B":
-        prompt("Current Balance: $", balance);
-        break;
-    }
-  } while (com != "Q");
-  exit;
+
+    } while (com != "Q");
+
+    exit;
+
 }
